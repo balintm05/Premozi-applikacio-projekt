fa41b03be7f0d9860cf9a3e37e35e2b0c5d4637c
ktjisebojtioeskotbwioi <nemlopottnevxd@gmail.com>
2025-04-11 21:40:56 +0200

    aaaa

--- premozi-complete.sql
+++ premozi-complete.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2025 at 11:19 AM
+-- Generation Time: Apr 11, 2025 at 09:40 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `premozi`
 --
-CREATE DATABASE IF NOT EXISTS `premozi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+CREATE DATABASE IF NOT EXISTS `premozi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `premozi`;
 
 -- --------------------------------------------------------
