9797968ff7019d84a2b48b6ec52418a8dbf7a27f
ktjisebojtioeskotbwioi <nemlopottnevxd@gmail.com>
2025-04-11 21:46:51 +0200

    aaa

--- premozi-dump-tablesonly.sql
+++ premozi-dump-tablesonly.sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 11, 2025 at 09:15 PM
+-- Generation Time: Apr 11, 2025 at 09:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -20,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `premozi`
 --
+CREATE DATABASE IF NOT EXISTS `premozi` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `premozi`;
 
 -- --------------------------------------------------------
 
@@ -27,11 +30,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `email2facodes`
 --
 
-CREATE TABLE `email2facodes` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `email2facodes`;
+CREATE TABLE IF NOT EXISTS `email2facodes` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Code` longtext NOT NULL,
   `UserID` int(11) NOT NULL,
-  `ExpiresAt` datetime(6) NOT NULL
+  `ExpiresAt` datetime(6) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Email2FACodes_UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -40,8 +46,9 @@ CREATE TABLE `email2facodes` (
 -- Table structure for table `film`
 --
 
-CREATE TABLE `film` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `film`;
+CREATE TABLE IF NOT EXISTS `film` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Cim` longtext NOT NULL,
   `Kategoria` longtext NOT NULL,
   `Mufaj` longtext NOT NULL,
@@ -57,7 +64,9 @@ CREATE TABLE `film` (
   `TrailerLink` longtext NOT NULL,
   `IMDB` longtext NOT NULL,
   `ImageID` int(11) NOT NULL,
-  `Megjegyzes` longtext NOT NULL
+  `Megjegyzes` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IX_Film_ImageID` (`ImageID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -66,10 +75,13 @@ CREATE TABLE `film` (
 -- Table structure for table `foglalasadatok`
 --
 
-CREATE TABLE `foglalasadatok` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `foglalasadatok`;
+CREATE TABLE IF NOT EXISTS `foglalasadatok` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `FoglalasIdopontja` datetime(6) NOT NULL,
-  `UserID` int(11) NOT NULL
+  `UserID` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IX_FoglalasAdatok_UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,12 +90,16 @@ CREATE TABLE `foglalasadatok` (
 -- Table structure for table `foglaltszekek`
 --
 
-CREATE TABLE `foglaltszekek` (
+DROP TABLE IF EXISTS `foglaltszekek`;
+CREATE TABLE IF NOT EXISTS `foglaltszekek` (
   `Vetitesid` int(11) NOT NULL,
   `X` int(11) NOT NULL,
   `Y` int(11) NOT NULL,
   `FoglalasAdatokid` int(11) NOT NULL,
-  `JegyTipusId` int(11) NOT NULL
+  `JegyTipusId` int(11) NOT NULL,
+  PRIMARY KEY (`FoglalasAdatokid`,`Vetitesid`,`X`,`Y`),
+  UNIQUE KEY `IX_FoglaltSzekek_Vetitesid_X_Y` (`Vetitesid`,`X`,`Y`),
+  KEY `IX_FoglaltSzekek_JegyTipusId` (`JegyTipusId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -92,8 +108,9 @@ CREATE TABLE `foglaltszekek` (
 -- Table structure for table `httplogs`
 --
 
-CREATE TABLE `httplogs` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `httplogs`;
+CREATE TABLE IF NOT EXISTS `httplogs` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Schema` longtext NOT NULL,
   `Host` longtext NOT NULL,
   `Path` longtext NOT NULL,
@@ -103,7 +120,8 @@ CREATE TABLE `httplogs` (
   `ResponseHeaders` longtext NOT NULL,
   `ResponseBody` longtext NOT NULL,
   `StatusCode` int(11) NOT NULL,
-  `LogTime` datetime(6) NOT NULL DEFAULT utc_timestamp()
+  `LogTime` datetime(6) NOT NULL DEFAULT utc_timestamp(),
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -112,14 +130,16 @@ CREATE TABLE `httplogs` (
 -- Table structure for table `images`
 --
 
-CREATE TABLE `images` (
-  `Id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
   `RelativePath` longtext NOT NULL,
   `FileName` longtext NOT NULL,
   `OriginalFileName` longtext NOT NULL,
   `ContentType` longtext NOT NULL,
   `FileSize` bigint(20) NOT NULL,
-  `UploadDate` datetime(6) NOT NULL
+  `UploadDate` datetime(6) NOT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -128,10 +148,12 @@ CREATE TABLE `images` (
 -- Table structure for table `jegytipus`
 --
 
-CREATE TABLE `jegytipus` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `jegytipus`;
+CREATE TABLE IF NOT EXISTS `jegytipus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nev` longtext NOT NULL,
-  `Ar` int(11) NOT NULL
+  `Ar` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -140,11 +162,13 @@ CREATE TABLE `jegytipus` (
 -- Table structure for table `szekek`
 --
 
-CREATE TABLE `szekek` (
+DROP TABLE IF EXISTS `szekek`;
+CREATE TABLE IF NOT EXISTS `szekek` (
   `X` int(11) NOT NULL,
   `Y` int(11) NOT NULL,
   `Teremid` int(11) NOT NULL,
-  `Allapot` int(11) NOT NULL
+  `Allapot` int(11) NOT NULL,
+  PRIMARY KEY (`Teremid`,`X`,`Y`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -153,10 +177,12 @@ CREATE TABLE `szekek` (
 -- Table structure for table `terem`
 --
 
-CREATE TABLE `terem` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `terem`;
+CREATE TABLE IF NOT EXISTS `terem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nev` longtext NOT NULL,
-  `Megjegyzes` longtext NOT NULL
+  `Megjegyzes` longtext NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -165,8 +191,9 @@ CREATE TABLE `terem` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `passwordHash` char(84) NOT NULL,
   `creationDate` datetime NOT NULL,
@@ -181,7 +208,10 @@ CREATE TABLE `users` (
   `TwoFactorEnabled` tinyint(1) NOT NULL,
   `PasswordResetToken` longtext DEFAULT NULL,
   `PasswordResetTokenExpiry` datetime(6) DEFAULT NULL,
-  `PasswordResetRequired` tinyint(1) NOT NULL
+  `PasswordResetRequired` tinyint(1) NOT NULL,
+  PRIMARY KEY (`userID`),
+  UNIQUE KEY `IX_Users_email` (`email`),
+  UNIQUE KEY `IX_Users_refreshToken` (`refreshToken`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -190,12 +220,16 @@ CREATE TABLE `users` (
 -- Table structure for table `vetites`
 --
 
-CREATE TABLE `vetites` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `vetites`;
+CREATE TABLE IF NOT EXISTS `vetites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `Idopont` datetime(6) NOT NULL,
   `Megjegyzes` longtext NOT NULL,
   `Filmid` int(11) NOT NULL,
-  `Teremid` int(11) NOT NULL
+  `Teremid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IX_Vetites_Filmid` (`Filmid`),
+  KEY `IX_Vetites_Teremid` (`Teremid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -204,11 +238,13 @@ CREATE TABLE `vetites` (
 -- Table structure for table `vetitesszekek`
 --
 
-CREATE TABLE `vetitesszekek` (
+DROP TABLE IF EXISTS `vetitesszekek`;
+CREATE TABLE IF NOT EXISTS `vetitesszekek` (
   `Vetitesid` int(11) NOT NULL,
   `X` int(11) NOT NULL,
   `Y` int(11) NOT NULL,
-  `FoglalasAllapot` int(11) NOT NULL
+  `FoglalasAllapot` int(11) NOT NULL,
+  PRIMARY KEY (`Vetitesid`,`X`,`Y`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -217,159 +253,12 @@ CREATE TABLE `vetitesszekek` (
 -- Table structure for table `__efmigrationshistory`
 --
 
-CREATE TABLE `__efmigrationshistory` (
+DROP TABLE IF EXISTS `__efmigrationshistory`;
+CREATE TABLE IF NOT EXISTS `__efmigrationshistory` (
   `MigrationId` varchar(150) NOT NULL,
-  `ProductVersion` varchar(32) NOT NULL
+  `ProductVersion` varchar(32) NOT NULL,
+  PRIMARY KEY (`MigrationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `email2facodes`
---
-ALTER TABLE `email2facodes`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `IX_Email2FACodes_UserID` (`UserID`);
-
---
--- Indexes for table `film`
---
-ALTER TABLE `film`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IX_Film_ImageID` (`ImageID`);
-
---
--- Indexes for table `foglalasadatok`
---
-ALTER TABLE `foglalasadatok`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IX_FoglalasAdatok_UserID` (`UserID`);
-
---
--- Indexes for table `foglaltszekek`
---
-ALTER TABLE `foglaltszekek`
-  ADD PRIMARY KEY (`FoglalasAdatokid`,`Vetitesid`,`X`,`Y`),
-  ADD UNIQUE KEY `IX_FoglaltSzekek_Vetitesid_X_Y` (`Vetitesid`,`X`,`Y`),
-  ADD KEY `IX_FoglaltSzekek_JegyTipusId` (`JegyTipusId`);
-
---
--- Indexes for table `httplogs`
---
-ALTER TABLE `httplogs`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`Id`);
-
---
--- Indexes for table `jegytipus`
---
-ALTER TABLE `jegytipus`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `szekek`
---
-ALTER TABLE `szekek`
-  ADD PRIMARY KEY (`Teremid`,`X`,`Y`);
-
---
--- Indexes for table `terem`
---
-ALTER TABLE `terem`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`),
-  ADD UNIQUE KEY `IX_Users_email` (`email`),
-  ADD UNIQUE KEY `IX_Users_refreshToken` (`refreshToken`);
-
---
--- Indexes for table `vetites`
---
-ALTER TABLE `vetites`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IX_Vetites_Filmid` (`Filmid`),
-  ADD KEY `IX_Vetites_Teremid` (`Teremid`);
-
---
--- Indexes for table `vetitesszekek`
---
-ALTER TABLE `vetitesszekek`
-  ADD PRIMARY KEY (`Vetitesid`,`X`,`Y`);
-
---
--- Indexes for table `__efmigrationshistory`
---
-ALTER TABLE `__efmigrationshistory`
-  ADD PRIMARY KEY (`MigrationId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `email2facodes`
---
-ALTER TABLE `email2facodes`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `film`
---
-ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `foglalasadatok`
---
-ALTER TABLE `foglalasadatok`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `httplogs`
---
-ALTER TABLE `httplogs`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `images`
---
-ALTER TABLE `images`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jegytipus`
---
-ALTER TABLE `jegytipus`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `terem`
---
-ALTER TABLE `terem`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `vetites`
---
-ALTER TABLE `vetites`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -419,6 +308,7 @@ ALTER TABLE `vetites`
 --
 ALTER TABLE `vetitesszekek`
   ADD CONSTRAINT `FK_VetitesSzekek_Vetites_Vetitesid` FOREIGN KEY (`Vetitesid`) REFERENCES `vetites` (`id`) ON DELETE CASCADE;
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
